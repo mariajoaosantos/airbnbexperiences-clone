4b2af57ed6232ceb06f223afee4c4ee8d432d7b7
mariajoaosantos <mariajcs18@gmail.com>
2022-06-16 15:36:30 +0100

    Static components ready

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img
+        className="hero--gallery"
+        src="/images/hero-gallery.png"
+        alt="Gallery"
+      ></img>
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
