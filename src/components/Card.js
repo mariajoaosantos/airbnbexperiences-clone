32b119e7aeb2299b0f9808c57b99258e41b02bad
mariajoaosantos <mariajcs18@gmail.com>
2022-07-08 21:24:19 +0100

    Cards added with map() function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,17 @@
 import React from "react";
 
 export default function Card(props) {
-  console.log(props)
   return (
     <div className="card">
       <img
         src={`./images/${props.img}`}
-        alt="Katie Zaferes"
         className="card--image"
       ></img>
       <div className="card--stats">
         <img src="./images/star.png" alt="Star" className="card--star"></img>
         <span>{props.rating}</span>
         <span className="grey">({props.reviewCount}) ·</span>
-        <span className="grey">{props.country}</span>
+        <span className="grey">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p>
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    id: 1,
+    title: "Life Lessons with Katie Zareferes",
+    description: "",
+    price: 136,
+    coverImg: "katie-zaferes.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+    location: "USA",
+    openSpots: "",
+  },
+  {
+    id: 2,
+    title: "Learn Wedding photography",
+    description: "",
+    price: 125,
+    coverImg: "wedding-photography.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+    location: "USA",
+    openSpots: "",
+  },
+  {
+    id: 2,
+    title: "Group Mountain Biking",
+    description: "",
+    price: 50,
+    coverImg: "mountain-bike.png",
+    stats: {
+      rating: 4.8,
+      reviewCount: 2,
+    },
+    location: "USA",
+    openSpots: "",
+  },
+];
